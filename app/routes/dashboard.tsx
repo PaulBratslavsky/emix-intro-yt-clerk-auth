febeba778e300e65de7839181cc51556b85186b3
Paul Bratslavsky <Paul.bratslavsky@gmail.com>
2024-01-10 11:37:41 -0600

    initial commit

---
+++ app/routes/dashboard.tsx
@@ -0,0 +1,21 @@
+import { UserButton } from "@clerk/remix";
+import { getAuth } from "@clerk/remix/ssr.server";
+import { LoaderFunction, redirect } from "@remix-run/node";
+
+export const loader: LoaderFunction = async (args) => {
+  const { userId } = await getAuth(args);
+  if (!userId) return redirect("/sign-in");
+  return {};
+};
+
+export default function DashboardRoute() {
+  return (
+    <div className="p-4">
+      <div className="flex justify-between items-center gap-4">
+      <h1>Dashboard Route - private </h1>
+      <UserButton afterSignOutUrl="/"/>
+      </div>
+
+    </div>
+  );
+}
